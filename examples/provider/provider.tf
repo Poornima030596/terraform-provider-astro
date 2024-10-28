893616aa2569e5cdefc600fcad5d2be3d83b966a
Poornima030596 <p.natarajshankar@elsevier.com>
2024-10-28 14:35:34 +0530

    Update provider.tf

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,13 @@
-provider "astro" {
-  organization_id = "cljzz64cc001n01mln1p12345"
+terraform {
+  required_providers {
+    astro = {
+      source  = "astronomer/astro"
+      version = "0.3.0"
+    }
+  }
 }
+
+provider "astro" {
+  organization_id = "cm1ojnif514gu01q1h6gi4m7e"
+}
+
